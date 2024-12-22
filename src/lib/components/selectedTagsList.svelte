e65b2a666000b187a6a2e2f40e52245ba532a964
Victor <victormovileanu@gmail.com>
2024-12-22 15:04:19 +0200

    WIP replace previously existing selectedTagsList

--- src/lib/components/selectedTagsList.svelte
+++ src/lib/components/selectedTagsList.svelte
@@ -1,61 +1,89 @@
 <script lang="ts">
-	import { tags } from '$lib/stores/tags.svelte';
+  import { tags } from '$lib/stores/tags.svelte';
+  import TagSlot from '$lib/components/TagSlot.svelte';
+  import MultiSelect, { type ObjectOption } from 'svelte-multiselect';
 
-	const { blobId, selectedTagIds, supabase } = $props();
+  const { blob, selectedTagIds, supabase } = $props();
+  const options: ObjectOption[] = $derived.by(() => {
+    return [...tags.values()].map(optionFromTag);
+  });
+  let selected: ObjectOption[] = $state([...tags.values()].filter(tag => selectedTagIds.has(tag.id)).map(optionFromTag));
 
-	async function removeTagFromBlob(tagId: number) {
-		const { error } = await supabase
-			.from('blob_tags')
-			.delete()
-			.match({ blob_id: blobId, tag_id: tagId });
-		// todo: add error message
-		if (error) {
-			console.error(error);
-		}
-		selectedTagIds.delete(tagId);
-	}
+  function optionFromTag(tag: Tag) {
+    return {
+      id: tag.id,
+      label: tag.name
+    };
+  }
+
+  async function onAdd(evt) {
+    let tagId = evt.detail.option.id;
+    if (!tagId) {
+      const name = evt.detail.option.label;
+      const { data: tag } = await supabase.from('tags').insert({ name: name }).select().maybeSingle();
+      evt.detail.option.id = tag.id;
+      tags.set(tag.id, tag);
+      tagId = tag.id;
+    }
+    selectedTagIds.add(tagId);
+    await supabase
+      .from('blob_tags')
+      .insert({ blob_id: blob.id, tag_id: tagId });
+  }
+
+  async function onRemove(evt) {
+    const tagId = evt.detail.option.id;
+    await supabase
+      .from('blob_tags')
+      .delete()
+      .match({ blob_id: blob.id, tag_id: tagId });
+    selectedTagIds.delete(tagId);
+  }
 </script>
 
 <div class="flex items-center gap-2">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke-width="1.5"
-		stroke="currentColor"
-		class="h-6 w-6"
-	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
-		/>
-		<path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-	</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="h-6 w-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
+    />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+  </svg>
 
-	{#if selectedTagIds.size > 0}
-		<ul class="flex flex-wrap gap-2">
-			{#each [...selectedTagIds].map((id) => tags.get(id)) as tag}
-				{#if tag}
-					<li class="badge">
-						{tag.name}
-						<button onclick={() => removeTagFromBlob(tag.id)} aria-label="unselect tag">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="size-4"
-							>
-								<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-							</svg>
-						</button>
-					</li>
-				{/if}
-			{/each}
-		</ul>
-	{:else}
-		no tags selected
-	{/if}
+  <MultiSelect
+    bind:selected
+    options={options}
+    key={opt => opt.id}
+    outerDivClass="border-none w-full [&_.remove-all]:hidden"
+    allowUserOptions="append"
+    --sms-placeholder-opacity="0.7"
+    placeholder="no tags selected"
+    selectedOptionsDraggable={false}
+    on:add={onAdd}
+    on:remove={onRemove}
+    let:option
+  >
+    <span slot="expand-icon"></span>
+    <span slot="remove-icon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+      </svg>
+    </span>
+    <TagSlot {option}></TagSlot>
+  </MultiSelect>
 </div>
--- src/routes/blob/[uuid]/+page.svelte
+++ src/routes/blob/[uuid]/+page.svelte
@@ -2,83 +2,22 @@
 	import SelectedTagsList from '$lib/components/selectedTagsList.svelte';
 	import TagList from '$lib/components/tagList.svelte';
 	import BlobUrl from '$lib/components/blobUrl.svelte';
-	import SearchTagsForm from '$lib/components/searchTagsForm.svelte';
 	import { SvelteSet } from 'svelte/reactivity';
-  import MultiSelect from 'svelte-multiselect';
-  import TagSlot from '$lib/components/TagSlot.svelte';
-  import { tags } from '$lib/stores/tags.svelte';
 
 
-	let { data, form } = $props();
+	let { data } = $props();
 
 	// https://svelte.dev/docs/kit/state-management#Using-stores-with-context
 	// das könnte ich machen, um nicht supabase überall einzutragen
 	const { supabase, blob, blobTags } = data;
-
-  function optionFromTag(tag: Tag) {
-    return {
-        id: tag.id,
-        label: tag.name
-      }
-  }
 	const selectedTagIds: SvelteSet<number> = new SvelteSet(blobTags);
-  const options = $derived.by(() => {
-    return [...tags.values()].map(optionFromTag)
-  })
-  let selected = $state([...tags.values()].filter(tag => selectedTagIds.has(tag.id)).map(optionFromTag))
-
-  async function onAdd(evt) {
-    let tagId = evt.detail.option.id
-    if (!tagId) {
-        const name = evt.detail.option.label
-        const { data: tag } = await supabase.from('tags').insert({ name: name }).select().maybeSingle()
-        evt.detail.option.id = tag.id
-        tags.set(tag.id, tag)
-        tagId = tag.id
-    }
-    selectedTagIds.add(tagId)
-    await supabase
-			.from('blob_tags')
-			.insert({ blob_id: blob.id, tag_id: tagId })
-  }
-
-  async function onRemove(evt) {
-    const tagId = evt.detail.option.id
-    await supabase
-			.from('blob_tags')
-			.delete()
-			.match({ blob_id: blob.id, tag_id: tagId });
-    selectedTagIds.delete(tagId)
-  }
 </script>
 
 <article>
 	<h1 class="mb-4 text-4xl">{blob.title}</h1>
 	<div class="flex flex-col gap-2">
-		<SelectedTagsList blobId={blob.id} {selectedTagIds} {supabase}></SelectedTagsList>
+		<SelectedTagsList {blob} {selectedTagIds} {supabase}></SelectedTagsList>
 		<BlobUrl {blob} />
-
-    <div class="my-4">
-      <MultiSelect
-        bind:selected
-        options={options}
-        key={opt => opt.id}
-        outerDivClass="border-none"
-        allowUserOptions="append"
-        --sms-placeholder-opacity="0.7"
-        placeholder="no tags selected"
-        on:add={onAdd}
-        on:remove={onRemove}
-        let:option
-      >
-        <span slot="expand-icon"></span>
-        <TagSlot {option}></TagSlot>
-      </MultiSelect>
-    </div>
-
-		<div class="mt-4">
-			<SearchTagsForm {selectedTagIds} {form} {supabase} blobId={blob.id} />
-		</div>
 	</div>
 </article>
 
