d73524b1707d934ab7cf40014e305c9b9d3c65f1
Victor <victormovileanu@gmail.com>
2024-12-22 14:37:17 +0200

    WIP works on adding and removing stuff

--- src/routes/blob/[uuid]/+page.svelte
+++ src/routes/blob/[uuid]/+page.svelte
@@ -4,6 +4,10 @@
 	import BlobUrl from '$lib/components/blobUrl.svelte';
 	import SearchTagsForm from '$lib/components/searchTagsForm.svelte';
 	import { SvelteSet } from 'svelte/reactivity';
+  import MultiSelect from 'svelte-multiselect';
+  import TagSlot from '$lib/components/TagSlot.svelte';
+  import { tags } from '$lib/stores/tags.svelte';
+
 
 	let { data, form } = $props();
 
@@ -11,7 +15,41 @@
 	// das könnte ich machen, um nicht supabase überall einzutragen
 	const { supabase, blob, blobTags } = data;
 
+  function optionFromTag(tag: Tag) {
+    return {
+        id: tag.id,
+        label: tag.name
+      }
+  }
 	const selectedTagIds: SvelteSet<number> = new SvelteSet(blobTags);
+  const options = $derived.by(() => {
+    return [...tags.values()].map(optionFromTag)
+  })
+  let selected = $state([...tags.values()].filter(tag => selectedTagIds.has(tag.id)).map(optionFromTag))
+
+  async function onAdd(evt) {
+    let tagId = evt.detail.option.id
+    if (!tagId) {
+        const name = evt.detail.option.label
+        const { data: tag } = await supabase.from('tags').insert({ name: name }).select().maybeSingle()
+        evt.detail.option.id = tag.id
+        tags.set(tag.id, tag)
+        tagId = tag.id
+    }
+    selectedTagIds.add(tagId)
+    await supabase
+			.from('blob_tags')
+			.insert({ blob_id: blob.id, tag_id: tagId })
+  }
+
+  async function onRemove(evt) {
+    const tagId = evt.detail.option.id
+    await supabase
+			.from('blob_tags')
+			.delete()
+			.match({ blob_id: blob.id, tag_id: tagId });
+    selectedTagIds.delete(tagId)
+  }
 </script>
 
 <article>
@@ -19,6 +57,24 @@
 	<div class="flex flex-col gap-2">
 		<SelectedTagsList blobId={blob.id} {selectedTagIds} {supabase}></SelectedTagsList>
 		<BlobUrl {blob} />
+
+    <div class="my-4">
+      <MultiSelect
+        bind:selected
+        options={options}
+        key={opt => opt.id}
+        outerDivClass="border-none"
+        allowUserOptions="append"
+        --sms-placeholder-opacity="0.7"
+        placeholder="no tags selected"
+        on:add={onAdd}
+        on:remove={onRemove}
+        let:option
+      >
+        <span slot="expand-icon"></span>
+        <TagSlot {option}></TagSlot>
+      </MultiSelect>
+    </div>
 
 		<div class="mt-4">
 			<SearchTagsForm {selectedTagIds} {form} {supabase} blobId={blob.id} />
