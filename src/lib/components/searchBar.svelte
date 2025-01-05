1c1ab4001bb3a5c3ce23f56e29a6ac117fb9fb06
Victor <victormovileanu@gmail.com>
2025-01-05 11:43:25 +0100

    make prettier

--- src/lib/components/searchBar.svelte
+++ src/lib/components/searchBar.svelte
@@ -26,8 +26,41 @@
 </script>
 
 <div class="p-2">
-  <label class="input input-bordered flex items-center cursor-text">
-    <input bind:this={searchInput} bind:value id="search-bar-input" class="outline-none" {onkeyup} autocomplete="off">
+  <label class="input input-bordered pr-0 flex cursor-text items-stretch">
+    <input
+      class="outline-none grow"
+      autocomplete="off"
+      placeholder="Search"
+      bind:this={searchInput}
+      bind:value
+      {onkeyup}
+      >
+
+    <button class="p-3 hover:bg-secondary/50 hover:text-secondary-content" title="Search" aria-label="Search">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+           class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+      </svg>
+
+    </button>
+
+    <button class="p-3 pr-4 hover:bg-secondary/50 hover:text-secondary-content rounded-e-full" title="Create" aria-label="Create">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+				/>
+			</svg>
+		</button>
   </label>
 </div>
 
